71b34d71590c7209b3a8b157075bcb9c2e113dce
NIRDESH KUMAR SAINI <saini.nirdesh.kumar@gmail.com>
2021-06-12 10:20:21 +0530

    ADD: Login

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -65,6 +65,7 @@ const ImageTitle = styled.div`
   min-height: 170px;
   width: 35vh;
   min-widht: 200px;
+  margin-top: 60px;
 
   img {
     width: 100%;
@@ -129,4 +130,5 @@ const Description = styled.div`
   font-size: 20px;
   padding-top: 16px;
   color: rgb(249, 249, 249);
+  max-width: 760px;
 `
